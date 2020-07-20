6b23b6abd62b12b382f840c2abf7b8695ed5537d
BNkosi <bulelaninkosi9@gmail.com>
2020-07-20 19:48:20 +0200

    requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license = 'MIT',
     description = 'Movie Recommender helper functions',
     long_description = open('README.md').read(),
-    install_requires = ['numpy', 'pandas', 'seaborn', 'random', 'cufflinks', 'sklearn', 'plotly.offline', 'scikit-surprise'],
+    install_requires = ['numpy', 'pandas', 'seaborn', 'cufflinks', 'sklearn', 'scikit-surprise'],
     url = 'https://github.com/Lizette95/unsupervised-predict-streamlit-template',
     author = 'JHB_Team_RM4',
     author_email = ['bulelaninkosi9@gmail.com'] # add emails
